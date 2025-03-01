8f62d4173dd4152a7c4bbee5815c5de751f84ab3
Saksham Kanojia <sakshamkanojia2002@gmail.com>
2025-03-01 14:53:34 +0530

    c9-updated code

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 import { AuthResponse, Category, User } from "@/types";
 import { getToken, handleApiError, setToken, setUser } from "@/lib/auth";
 
-const API_URL =  "https://ecomm-sk-backend.vercel.app/api";
-// "http://localhost:5000/api";
+const API_URL = "http://localhost:5000/api";
 
 
 const api = axios.create({
